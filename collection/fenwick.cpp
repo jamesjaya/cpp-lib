ab3444ec0a15a9faefb3caf326caf83216a2baa9
James Jaya <tjong.james@gmail.com>
2017-06-18 12:29:15 +0200

    Convert Fenwick to cpp template

--- collection/fenwick.cpp
+++ collection/fenwick.cpp
@@ -2,15 +2,16 @@
 
 using namespace std;
 
+template <class T>
 class Fenwick {
 
 private:
-	vector<int> data;
-	vector<int> dataMul;
+	vector<T> data;
+	vector<T> dataMul;
 	int len;
 	bool range;
 
-	void internalUpdate(int at, int mul, int add) {
+	void internalUpdate(int at, T mul, T add) {
 		while (at < len) {
 			dataMul[at] += mul;
 			data[at] += add;
@@ -18,8 +19,8 @@ private:
 		}
 	}
 
-	int queryStd(int at) {
-		int res = 0;
+	T queryStd(int at) {
+		T res = 0;
 		while (at >= 0) {
 			res += data[at];
 			at = (at & (at + 1)) - 1;
@@ -27,9 +28,9 @@ private:
 		return res;
 	}
 
-	int queryIfRange(int at) {
-		int mul = 0;
-		int add = 0;
+	T queryIfRange(int at) {
+		T mul = 0;
+		T add = 0;
 		int start = at;
 
 		while (at >= 0) {
@@ -48,7 +49,7 @@ public:
 		if (range) dataMul.assign(len + 1, 0);
 	}
 
-	void update(int at, int by) {
+	void update(int at, T by) {
 		if (range) throw;
 		while (at < len) {
 			data[at] += by;
@@ -56,21 +57,21 @@ public:
 		}
 	}
 
-	void updateRange(int left, int right, int by) {
+	void updateRange(int left, int right, T by) {
 		if (!range) throw;
-		internalUpdate(left, by, -by * (left -1));
+		internalUpdate(left, by, -by * (left - 1));
 		internalUpdate(right, -by, by * right);
 	}
 
-	int query(int at) {
+	T query(int at) {
 		return range ? queryIfRange(at) : queryStd(at);
 	}
 
-	int queryRange(int l, int r) {
-		return query(r) - query(l-1);
+	T queryRange(int left, int right) {
+		return query(right) - query(left - 1);
 	}
 };
 
 int main() {
-	
+	Fenwick<int> fen(10);
 }
