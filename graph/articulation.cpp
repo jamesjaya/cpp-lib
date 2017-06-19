539b89eb7b95cc111adf447bbf402497ba0f91d5
James Jaya <tjong.james@gmail.com>
2017-06-19 20:25:00 +0200

    Add Articulation

---
+++ graph/articulation.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+class Articulation {
+private:
+	vector<int> depth;
+	vector<int> low;
+	vector<int> parent;
+	vector<int>* g;
+	int n;
+
+	void dfs(int cur, int d) {
+		low[cur] = depth[cur] = d;
+		int children = 0;
+		bool isArticulation = false;
+
+		for (int next: g[cur]) {
+			if (depth[next] == -1) {
+				parent[next] = cur;
+				children++;
+				dfs(next, d + 1);
+
+				if (low[next] >= depth[cur]) {
+					isArticulation = true;
+					if (low[next] != depth[cur]) {
+						bridges.emplace_back(min(cur, next), max(cur, next));
+					}
+				}
+
+				low[cur] = min(low[cur], low[next]);
+			} else if (next != parent[cur]) {
+				low[cur] = min(low[cur], depth[next]);
+			}
+		}
+
+		if ((parent[cur] != -1 && isArticulation) || (parent[cur] == -1 && children > 1)) {
+			points.push_back(cur);
+		}
+	}
+
+public:
+	vector<int> points;
+	vector<ii> bridges;
+
+	Articulation(int n, vector<int>* adjList) {
+		parent.assign(n, -1);
+		depth.assign(n, -1);
+		low.assign(n, 0);
+		g = adjList;
+
+		for (int i = 0; i < n; i++) {
+			if (depth[i] == -1) {
+				dfs(i, 0);
+			}
+		}
+	}
+};
+
+int main() {
+	vector<int> adj[100];
+	adj[0].push_back(1);
+	adj[1].push_back(0);
+	adj[1].push_back(2);
+	adj[2].push_back(1);
+	adj[2].push_back(0);
+	adj[0].push_back(2);
+	adj[2].push_back(3);
+	adj[3].push_back(2);
+	adj[3].push_back(4);
+	adj[4].push_back(3);
+	adj[4].push_back(5);
+	adj[5].push_back(4);
+	adj[5].push_back(3);
+	adj[3].push_back(5);
+
+	Articulation articulation(6, adj);
+	for (int point: articulation.points) {
+		cout << point << '\n';
+	}
+
+	for (ii bridge: articulation.bridges) {
+		cout << bridge.first << ' ' << bridge.second << '\n';
+	}
+}
