4ce467952436cd8991101ee7ebfd51d1861eb261
James Jaya <james.jaya@booking.com>
2017-06-21 16:35:19 +0200

    Reindent UnionFind

--- collection/unionfind.cpp
+++ collection/unionfind.cpp
@@ -3,47 +3,47 @@
 using namespace std;
 
 struct UnionFind {
-  vector<int> p, rank, setSize;
-  int numSets;
+	vector<int> p, rank, setSize;
+	int numSets;
 
-  UnionFind(int N) {
-    setSize.assign(N, 1);
-    numSets = N;
-    rank.assign(N, 0);
-    p.assign(N, 0);
-    for (int i = 0; i < N; i++) p[i] = i;
-  }
-  
-  int findSet(int i) {
-    return (p[i] == i) ? i : (p[i] = findSet(p[i]));
-  }
+	UnionFind(int N) {
+		setSize.assign(N, 1);
+		numSets = N;
+		rank.assign(N, 0);
+		p.assign(N, 0);
+		for (int i = 0; i < N; i++) p[i] = i;
+	}
+	
+	int findSet(int i) {
+		return (p[i] == i) ? i : (p[i] = findSet(p[i]));
+	}
 
-  bool isSameSet(int i, int j) {
-    return findSet(i) == findSet(j);
-  }
-  
-  void unionSet(int i, int j) { 
-    if (!isSameSet(i, j)) {
-      numSets--; 
-      int x = findSet(i), y = findSet(j);
-      if (rank[x] > rank[y]) {
-        p[y] = x; setSize[x] += setSize[y];
-      } else {
-        p[x] = y; setSize[y] += setSize[x];
-        if (rank[x] == rank[y])
-          rank[y]++;
-      }
-    }
-  }
-  int numDisjointSets() {
-    return numSets;
-  }
+	bool isSameSet(int i, int j) {
+		return findSet(i) == findSet(j);
+	}
+	
+	void unionSet(int i, int j) { 
+		if (!isSameSet(i, j)) {
+			numSets--; 
+			int x = findSet(i), y = findSet(j);
+			if (rank[x] > rank[y]) {
+				p[y] = x; setSize[x] += setSize[y];
+			} else {
+				p[x] = y; setSize[y] += setSize[x];
+				if (rank[x] == rank[y])
+					rank[y]++;
+			}
+		}
+	}
+	int numDisjointSets() {
+		return numSets;
+	}
 
-  int sizeOfSet(int i) {
-    return setSize[findSet(i)];
-  }
+	int sizeOfSet(int i) {
+		return setSize[findSet(i)];
+	}
 };
 
 int main() {
-  
+	
 }
