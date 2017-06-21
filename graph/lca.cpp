629a955cd66649ed64ea04f49845278250531cd1
James Jaya <tjong.james@gmail.com>
2017-06-21 08:23:05 +0200

    Add forest support to LCA

--- graph/lca.cpp
+++ graph/lca.cpp
@@ -63,17 +63,19 @@ private:
 	int n;
 	int idx;
 	vector<vector<int>> g;
-	vector<int> l, e, h;
+	vector<int> l, e, h, tn;
 	RMQ<int> tl;
 
-	void dfs(int cur, int prev, int depth) {
+	void dfs(int cur, int depth, int number) {
+		tn[cur] = number;
 		h[cur] = idx;
+		d[cur] = depth;
 		e[idx] = cur;
 		l[idx++] = depth;
 
 		for (int next: g[cur]) {
-			if (next != prev) {
-				dfs(next, cur, depth + 1);
+			if (h[next] == -1) {
+				dfs(next, depth + 1, number);
 				e[idx] = cur;
 				l[idx++] = depth;
 			}
@@ -81,23 +83,38 @@ private:
 	}
 
 public:
-	LCA(int numNodes, vector<vector<int>>& adj) : l(2 * numNodes), e(2 * numNodes), h(numNodes) {
+	vector<int> d;
+
+	LCA(int numNodes, vector<vector<int>>& adj) : l(2 * numNodes + 1), e(2 * numNodes + 1), h(numNodes, -1), d(numNodes, 0), tn(numNodes, 0) {
 		n = numNodes;
 		g = adj;
 		idx = 0;
+		int number = 0;
 
-		dfs(0, -1, 0);
-		tl = RMQ<int>(2 * n, l);
+		for (int i = 0; i < n; i++) {
+			if (h[i] == -1) {
+				dfs(i, 0, number++);
+			}
+		}
+		
+		tl = RMQ<int>(2 * n + 1, l);
 	}
 
 	int query(int a, int b) {
+		if (tn[a] != tn[b]) return -1;
 		if (h[b] < h[a]) swap(a, b);
 		return e[tl.query(h[a], h[b])];
+	}
+
+	int dist(int a, int b) {
+		if (tn[a] != tn[b]) return -1;
+		int lca = query(a, b);
+		return d[a] + d[b] - 2 * d[lca];
 	}
 };
 
 int main() {
-	vector<vector<int>> tree(10);
+	vector<vector<int>> tree(15);
 	tree[0].push_back(1);
 	tree[0].push_back(2);
 	tree[1].push_back(3);
@@ -107,12 +124,16 @@ int main() {
 	tree[2].push_back(7);
 	tree[6].push_back(8);
 	tree[8].push_back(9);
+	tree[10].push_back(11);
+	tree[11].push_back(12);
 
-	LCA lca(10, tree);
+	LCA lca(15, tree);
 	cout << lca.query(5, 9) << '\n';
 	cout << lca.query(3, 4) << '\n';
 	cout << lca.query(1, 2) << '\n';
 	cout << lca.query(0, 9) << '\n';
 	cout << lca.query(7, 2) << '\n';
 	cout << lca.query(2, 7) << '\n';
+	cout << lca.query(10, 11) << '\n';
+	cout << lca.query(10, 2) << '\n';
 }
