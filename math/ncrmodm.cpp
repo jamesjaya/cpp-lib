a6ee7176ceb4732ed33f16e4eea7665be0242599
James Jaya <james.jaya@booking.com>
2017-06-27 14:34:09 +0200

    Add groundwork for generalized lucas

--- math/ncrmodm.cpp
+++ math/ncrmodm.cpp
@@ -1,9 +1,28 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+#include <bitset>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 typedef long long ll;
 typedef pair<int, int> ii;
+
+bitset<100000> bs;
+vector<ll> primes;
+
+void sieve(ll sz) {
+    bs[0] = bs[1] = 1;
+    for (ll i = 2; i <= sz; i++) {
+        if (!bs[i]) {
+            for (ll j = i*i; j <= sz; j+=i) {
+                bs[j] = 1;
+            }
+            primes.push_back(i);
+        }
+    }
+}
 
 ll fex(ll base, ll exp, ll mod) {
 	if (exp == 1) return base;
@@ -22,12 +41,49 @@ ll invmod(ll operand, ll mod) {
 	return fex(operand, mod - 2, mod);
 }
 
-struct Lucas {
-	ll m, sz;
-	vector<ll> fact, ifact;
+vector<ii> primeFactorialization(ll num) {
+    vector<ii> ret;
+    int p = 0;
+    while (num > 1) {
+        int cnt = 0;
+        while (num % primes[p] == 0) {
+            num /= primes[p];
+            cnt++;
+        }
+        if (cnt) ret.emplace_back(primes[p], cnt);
+        p++;
+    }
+    return ret;
+}
 
-	Lucas(ll m, ll sz) : m(m), sz(sz), fact(sz), ifact(sz) {
+ll chinese_remainder(vector<ll>& mods, vector<ll>& rems) {
+    ll prod = 1, sum = 0;
+    
+    for (ll mod: mods) {
+        prod *= mod;
+    }
+    for (int i = 0; i < (int) rems.size(); i++) {
+        ll p = prod / mods[i];
+        sum += rems[i] * invmod(p, mods[i]) * p;
+    }
+
+    return sum % prod;
+}
+
+struct Lucas {
+	ll m, k, sz, pw;
+	vector<ll> fact, ifact;
+    const static ll maxSz = 100000;
+
+	Lucas(ll m, ll k) : m(m), k(k), sz(min(m, 100000ll)), fact(min(m, 100000ll)), ifact(min(m, 100000ll)) {
 		generateFact();
+        if (m == 2) {
+            pw = k / 2;
+        } else if (m == 3) {
+            pw = (k - 1) / 2;
+        } else {
+            pw = (k - 1) / 3;
+        }
 	}
 
 	void generateFact() {
@@ -46,12 +102,13 @@ struct Lucas {
 	}
 
 	vector<ll> decompose(ll num) {
-		vector<ll> ret;
-		while (num > 0) {
-			ret.push_back(num % m);
-			num /= m;
-		}
-		return ret;
+        vector<ll> ret;
+        bool first = true;
+        while (num > 0) {
+            ret.push_back((num % m) * fex(m, pw, 1e9+7));
+            num /= m;
+        }
+        return ret;
 	}
 
 	ll calculate(ll n, ll c) {
@@ -69,7 +126,25 @@ struct Lucas {
 	}
 };
 
+struct LucasAux {
+    static ll process(ll n, ll r, ll m) {
+        vector<ii> pfactors = primeFactorialization(m);
+
+        vector<ll> mods, rems;
+        for (ii pfactor: pfactors) {
+            mods.push_back(fex(pfactor.first, pfactor.second, m));
+
+            Lucas lucas(pfactor.first, pfactor.second);
+            rems.push_back(lucas.calculate(n, r));
+        }
+
+        return chinese_remainder(mods, rems);
+    }
+};
+
 int main() {
+    sieve(100000);
+
 	// Sanity test
 	cout << "Sanity test, mod 1e9+7\n";
 	Lucas lucas(1e9+7, 10);
@@ -85,4 +160,6 @@ int main() {
 	cout << "C(6,3) = 20 % 3 = 2 ----> " << lucas2.calculate(6, 3) << '\n';
 	cout << "C(3,0) =  1 % 3 = 1 ----> " << lucas2.calculate(3, 0) << '\n';
 	cout << "C(3,1) =  3 % 3 = 0 ----> " << lucas2.calculate(3, 1) << '\n';
+
+    cout << LucasAux::process(5, 2, 30) << '\n';
 }
